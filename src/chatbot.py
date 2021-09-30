a90f7c178a25a67e30a88fa5adb2e9e785545e91
Biogitte <birgittenilsson@live.dk>
2021-09-30 21:45:12 +0200

    Add quit functionality

--- src/chatbot.py
+++ src/chatbot.py
@@ -5,12 +5,17 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
+import argparse
+import json
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 class ChatBot:
     """ Chatbot. """
 
-    def __init__(self, model_path: str, words_path: str, classes_path: str):
+    def __init__(self, model_path: str, words_path: str, classes_path: str, intents: str):
         # TODO: check and clean-up type hints
         # TODO: Add missing docstrings and improve existing
         self.model_path = model_path
@@ -20,8 +25,16 @@ class ChatBot:
         self.model = load_model(self.model_path)
         self.words = pickle.load(open(self.words_path, 'rb'))
         self.classes = pickle.load(open(self.classes_path, 'rb'))
+        self.intents = intents
+        if intents.endswith(".json"):
+            self.import_json(intents)
 
         self.lemmatizer = WordNetLemmatizer()
+
+    def import_json(self, intents: str):
+        # TODO: add description of JSON structure used for training data in docstring.
+        """Import JSON file containing intents."""
+        self.intents = json.loads(open(intents).read())
 
     def clean_sentence(self, sentence: str):
         sentence_words = nltk.word_tokenize(sentence)
@@ -66,9 +79,23 @@ class ChatBot:
         return result
 
     def chatbot(self):
-        """ """
         while True:
-            message = input("")
-            ints = self.predict_category(message)
-            res = self.get_response(ints, intents)
-            print(res)
+            message = input("Please, enter your first aid question (write bye to exit): ")
+            if message == 'Bye' or message == 'bye':
+                print('Thank you for visiting. Bye.')
+                break
+            else:
+                ints = self.predict_category(message)
+                res = self.get_response(ints, self.intents)
+                print(res)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', help='Path to Chatbot model (i.e., h5) file.', type=str)
+    parser.add_argument('--words_path', help='Path to pickle file with Chatbot words.', type=str)
+    parser.add_argument('--classes_path', help='Path to pickle file with Chatbot classes.', type=str)
+    parser.add_argument('--json_file', help='Input JSON file.', type=str)
+    args = parser.parse_args()
+    bot = ChatBot(args.model_path, args.words_path, args.classes_path, args.json_file)
+    bot.chatbot()
