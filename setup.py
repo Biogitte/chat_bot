9e06d28a4f2e5200c4d9d9a0f6abdd7373fcce8f
Biogitte <birgittenilsson@live.dk>
2021-09-30 20:27:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    package_dir={'src': 'src'},
+    packages=find_packages(),
+    version='0.1.0',
+    description='First Aid Chatbot',
+    author='Birgitte Nilsson',
+    license='MIT')
+
+find_packages()
